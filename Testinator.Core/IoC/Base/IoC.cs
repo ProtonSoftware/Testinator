b9d1d088b790d3a5cc2b3312ab838d616cebfeb7
Hooterr <maxus1999@gmail.com>
2017-11-30 21:45:15 +0100

    Adding base classes to Testinator.Core
    Added base classes like BaseViewModel to the Testinator.Core project.

---
+++ Testinator.Core/IoC/Base/IoC.cs
@@ -0,0 +1,62 @@
+﻿using Ninject;
+using System;
+
+namespace Testinator.Core
+{
+    /// <summary>
+    /// The IoC container for our application
+    /// </summary>
+    public static class IoC
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The kernel for our IoC container
+        /// </summary>
+        public static IKernel Kernel { get; private set; } = new StandardKernel();
+
+        /// <summary>
+        /// A shortcut to access the <see cref="IUIManager"/>
+        /// </summary>
+        /// Just an exaple
+        /// Can be deleted anytime
+        //public static IUIManager UI => IoC.Get<IUIManager>();
+
+        #endregion
+
+        #region Construction
+
+        /// <summary>
+        /// Sets up the IoC container, binds all information required and is ready for use
+        /// NOTE: Must be called as soon as your application starts up to ensure all 
+        ///       services can be found
+        /// </summary>
+        public static void Setup()
+        {
+            // Bind all required view models
+            BindViewModels();
+        }
+
+        /// <summary>
+        /// Binds all singleton view models
+        /// </summary>
+        private static void BindViewModels()
+        {
+            // How to bind a view model
+            // Just an example
+            //Kernel.Bind<SettingsViewModel>().ToConstant(new SettingsViewModel());
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Get's a service from the IoC, of the specified type
+        /// </summary>
+        /// <typeparam name="T">The type to get</typeparam>
+        /// <returns></returns>
+        public static T Get<T>()
+        {
+            return Kernel.Get<T>();
+        }
+    }
+}
