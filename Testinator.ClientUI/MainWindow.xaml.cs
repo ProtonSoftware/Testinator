a3d2823d524687fe5ab63d40d21e30f48bfd5a0d
Hooterr <maxus1999@gmail.com>
2017-11-30 21:33:43 +0100

    Adding base projects and changing framework to 4.6.2
    Added base projects and changed target .net framework to 4.6.2.

--- Testinator.ClientUI/MainWindow.xaml.cs
+++ Testinator.ClientUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Testinator.ClientUI
+namespace Testinator.Client
 {
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
---
+++ Testinator.Network.Client/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testinator.Network.Client
+{
+    public class Class1
+    {
+    }
+}
