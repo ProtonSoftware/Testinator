666af889c22a06c87ec5130f2c88e6b13cf78438
Patryk Mikulski <minorsonek@gmail.com>
2017-12-01 17:41:04 +0100

    Added .UICore and base window UI for both apps

---
+++ Source/Testinator.Server/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Testinator.Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set DataContext to the WindowViewModel to allow binding in xaml
+            DataContext = new WindowViewModel(this);
+        }
+    }
+}
