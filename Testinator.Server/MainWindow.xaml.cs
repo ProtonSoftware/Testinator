8f16adf63e0daacbd22c679f47fb62b2f73c36ea
Hooterr <maxus1999@gmail.com>
2017-12-01 18:15:09 +0100

    Added tester window in Testinator.Server
    Temporary used Testinator.Server to test the communication.

--- Testinator.Server/MainWindow.xaml.cs
+++ Testinator.Server/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Testinator.Network.Server;
 
 namespace Testinator.Server
 {
@@ -20,9 +21,39 @@ namespace Testinator.Server
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public ServerBase Server { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Server = new ServerBase(txtIp.Text, Int32.Parse(txtPort.Text))
+            {
+                ReceiverCallback = Receive
+            };
+            Server.Start();
+
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+        }
+
+        private void Receive(byte[] data)
+        {
+            string msg = Encoding.ASCII.GetString(data);
+            Dispatcher.Invoke(() => { txtRec.Text += msg + "\n"; });
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+
+            Server.Stop();
         }
     }
 }
