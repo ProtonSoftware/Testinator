38025db98e8d378f5f97ff77167c38c649a7925c
Hooterr <maxus1999@gmail.com>
2017-12-02 20:50:03 +0100

    Added test project to test Client communication.

---
+++ Source/ClientTesting/ViewModelClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+using Testinator.Core;
+
+namespace ClientTesting
+{
+    public class ViewModelClient : BaseViewModel
+    {
+        public string Ip { get; set; } = "127.0.0.1";
+
+        public int Port { get; set; } = 3333;
+
+        public ICommand StartCommand { get; set; }
+
+        public ICommand StopCommand { get; set; }
+
+        public bool StartPossible { get; set; } = true;
+
+        public bool StopPossible { get; set; } = false;
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ViewModelClient()
+        {
+            StartCommand = new RelayCommand(Start);
+            StopCommand = new RelayCommand(Stop);
+                
+        }
+
+        private void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
