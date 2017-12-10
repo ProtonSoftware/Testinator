7d5c25880c6efe1792f3061c1649705b0ce688bf
Patryk Mikulski <minorsonek@gmail.com>
2017-12-10 21:01:53 +0100

    TestEditor Question Add mechanic

--- Source/Testinator.Server.Core/ViewModels/Pages/TestEditorViewModel.cs
+++ Source/Testinator.Server.Core/ViewModels/Pages/TestEditorViewModel.cs
@@ -71,11 +71,27 @@ namespace Testinator.Server.Core
         public string AnswerD { get; set; }
         public string AnswerE { get; set; }
 
-        public int HowManyAnswersVisible = 2;
+        public int HowManyMultipleChoiceAnswersVisible = 2;
 
         public bool ShouldAnswerCBeVisible { get; set; } = false;
         public bool ShouldAnswerDBeVisible { get; set; } = false;
         public bool ShouldAnswerEBeVisible { get; set; } = false;
+
+        #endregion
+
+        #region Multiple Checkboxes Answers
+
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+
+        public int HowManyMultipleCheckboxesAnswersVisible = 2;
+
+        public bool ShouldAnswer3BeVisible { get; set; } = false;
+        public bool ShouldAnswer4BeVisible { get; set; } = false;
+        public bool ShouldAnswer5BeVisible { get; set; } = false;
 
         #endregion
 
@@ -94,9 +110,9 @@ namespace Testinator.Server.Core
         public ICommand SubmitQuestionCommand { get; private set; }
 
         /// <summary>
-        /// The command to add new answer to the multiple choice question
+        /// The command to add new answer to the question
         /// </summary>
-        public ICommand AddAnswerMultipleChoiceCommand { get; private set; }
+        public ICommand AddAnswerCommand { get; private set; }
 
         #endregion
 
@@ -110,7 +126,7 @@ namespace Testinator.Server.Core
             // Create commands
             AddingQuestionsPageChangeCommand = new RelayCommand(ChangePage);
             SubmitQuestionCommand = new RelayCommand(SubmitQuestion);
-            AddAnswerMultipleChoiceCommand = new RelayCommand(AddAnswerMultipleChoice);
+            AddAnswerCommand = new RelayCommand(AddAnswer);
         }
 
         #endregion
@@ -146,18 +162,42 @@ namespace Testinator.Server.Core
         /// <summary>
         /// Adds answer to the multiple choice question
         /// </summary>
-        private void AddAnswerMultipleChoice()
+        private void AddAnswer()
         {
-            // Show next answer
-            HowManyAnswersVisible++;
-            switch(HowManyAnswersVisible)
+            // Check which question type is being created
+            if (QuestionBeingAddedType == QuestionType.MultipleChoice)
             {
-                case 3: ShouldAnswerCBeVisible = true;
-                    break;
-                case 4: ShouldAnswerDBeVisible = true;
-                    break;
-                case 5: ShouldAnswerEBeVisible = true;
-                    break;
+                // Show next answer
+                HowManyMultipleChoiceAnswersVisible++;
+                switch (HowManyMultipleChoiceAnswersVisible)
+                {
+                    case 3:
+                        ShouldAnswerCBeVisible = true;
+                        break;
+                    case 4:
+                        ShouldAnswerDBeVisible = true;
+                        break;
+                    case 5:
+                        ShouldAnswerEBeVisible = true;
+                        break;
+                }
+            }
+            else if (QuestionBeingAddedType == QuestionType.MultipleCheckboxes)
+            {
+                // Show next answer
+                HowManyMultipleCheckboxesAnswersVisible++;
+                switch (HowManyMultipleCheckboxesAnswersVisible)
+                {
+                    case 3:
+                        ShouldAnswer3BeVisible = true;
+                        break;
+                    case 4:
+                        ShouldAnswer4BeVisible = true;
+                        break;
+                    case 5:
+                        ShouldAnswer5BeVisible = true;
+                        break;
+                }
             }
         }
 
@@ -170,172 +210,5 @@ namespace Testinator.Server.Core
         }
 
         #endregion
-
-        // Test/Dummy/Placeholder data
-        /*
-        public string Name { get; set; } = "nazwa";
-        public string Duration { get; set; } = "32";
-        public ICommand SubmitCommand { get; set; }
-
-        public string Pytanie1 { get; set; } = "dddd?";
-        public string A1 { get; set; } = "sa";
-        public string B1 { get; set; } = "sa";
-        public string C1 { get; set; } = "ds";
-        public string D1 { get; set; } = "3";
-        public string PoprawnaIdx1 { get; set; } = "1";
-        public string Punkty1 { get; set; } = "1";
-
-        public string Pytanie2 { get; set; } = "sdsdsd";
-        public string Odp2 { get; set; } = "sss";
-        public string Punkty2 { get; set; } = "1";
-
-        public string Pytanie3 { get; set; } = "adsfdasf";
-        public string A3 { get; set; } = "fdsf";
-        public string B3 { get; set; } = "dfdsf";
-        public string C3 { get; set; } = "sdfsd";
-        public string D3 { get; set; } = "fsdfs";
-        public string PoprawnaIdx3 { get; set; } = "1";
-        public string Punkty3 { get; set; } = "12";
-
-        public string Pytanie4 { get; set; } = "agsdfdg";
-        public bool A4 { get; set; } = true;
-        public bool B4 { get; set; } = false;
-        public bool C4 { get; set; } = true;
-        public string OptionA4 { get; set; } = "opcja1";
-        public string OptionB4 { get; set; } = "opcja2";
-        public string OptionC4 { get; set; } = "opcja3";
-        public string Punkty4 { get; set; } = "12";
-
-        public string Pytanie5 { get; set; } = "dfklsdf";
-        public string A5 { get; set; } = "asas";
-        public string B5 { get; set; } = "sas";
-        public string C5 { get; set; } = "ggg";
-        public string D5 { get; set; } = "ffdf";
-        public string PoprawnaIdx5 { get; set; } = "2";
-        public string Punkty5 { get; set; } = "11";
-
-        private void Submit()
-        {
-            var time = new TimeSpan();
-            int pkt1;
-            int pkt2;
-            int pkt3;
-            int pkt4;
-            int pkt5;
-                    
-            int pop1;
-            int pop3;
-            int pop5;
-            
-            try
-            {
-                time = new TimeSpan(0, Int32.Parse(Duration), 0);
-                pkt1 = Int32.Parse(Punkty1);
-                pkt2 = Int32.Parse(Punkty1);
-                pkt3 = Int32.Parse(Punkty1);
-                pkt4 = Int32.Parse(Punkty1);
-                pkt5 = Int32.Parse(Punkty1);
-
-                pop1 = Int32.Parse(PoprawnaIdx1);
-                pop3 = Int32.Parse(PoprawnaIdx3);
-                pop5 = Int32.Parse(PoprawnaIdx5);
-
-            }
-            catch
-            {
-                return;
-            }
-
-            var q1 = new MultipleChoiceQuestion();
-            var q2 = new SingleTextBoxQuestion();
-            var q3 = new MultipleChoiceQuestion();
-            var q4 = new MultipleCheckboxesQuestion();
-            var q5 = new MultipleChoiceQuestion();
-
-            try
-            {
-                q1 = new MultipleChoiceQuestion()
-                {
-                    PointScore = pkt1,
-                    Task = Pytanie1,
-                    CorrectAnswerIndex = pop1,
-                    Options = new List<string>() { A1, B1, C1, D1},
-                };
-            }
-            catch (QuestionException ex)
-            {
-            }
-
-            try
-            {
-                q2 = new SingleTextBoxQuestion()
-                {
-                    PointScore = pkt2,
-                    Task = Pytanie2,
-                    CorrectAnswer = "pomidor",
-                };
-            }
-            catch (QuestionException ex)
-            {
-            }
-
-            try
-            {
-                q3 = new MultipleChoiceQuestion()
-                {
-                    PointScore = pkt3,
-                    Task = Pytanie3,
-                    CorrectAnswerIndex = pop3,
-                    Options = new List<string>() { A3, B3, C3, D3 },
-                };
-            }
-            catch (QuestionException ex)
-            {
-            }
-
-            try
-            {
-                q4 = new MultipleCheckboxesQuestion()
-                {
-                    PointScore = pkt4,
-                    Task = Pytanie4,
-                    OptionsAndAnswers = new Dictionary<string, bool>()
-                    {
-                        { OptionA4, A4 },
-                        { OptionB4, B4 },
-                        { OptionC4, C4 },
-                    },
-                
-                };
-            }
-            catch (QuestionException ex)
-            {
-            }
-
-            try
-            {
-                q5 = new MultipleChoiceQuestion()
-                {
-                    PointScore = pkt5,
-                    Task = Pytanie5,
-                    CorrectAnswerIndex = pop5,
-                    Options = new List<string>() { A5, B5, C5, D5 },
-                };
-            }
-            catch (QuestionException ex)
-            {
-            }
-
-            var test = new Test()
-            {
-                Name = Name,
-                Duration = time,
-                Questions = new List<Question>() { q1, q2, q3, q4 }
-            };
-            test.AddQuestion(q5);
-        }
-
-    
-        */
     }
 }
