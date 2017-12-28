b8d90cccdc46092869916fa78bf7f2287e5ef7a9
Patryk Mikulski <minorsonek@gmail.com>
2017-12-28 18:02:24 +0100

    Result page mark calculation

--- Source/Testinator.Client.Core/ViewModels/Pages/ResultViewModel.cs
+++ Source/Testinator.Client.Core/ViewModels/Pages/ResultViewModel.cs
@@ -32,6 +32,16 @@ namespace Testinator.Client.Core
         /// </summary>
         public TimeSpan CompletionTime => Test.Duration - IoCClient.Application.TimeLeft;
 
+        /// <summary>
+        /// The score user achieved
+        /// </summary>
+        public int UserScore { get; set; }
+
+        /// <summary>
+        /// The mark user has achieved by doing the test
+        /// </summary>
+        public Marks UserMark => Test.Grading.GetMark(UserScore);
+
         #endregion
 
         #region Commands
@@ -52,6 +62,9 @@ namespace Testinator.Client.Core
         {
             // Load data from TestHost
             LoadTestHostData();
+
+            // Calculate user's score
+            UserScore = CompareUserAnswersAndCalculatePoints();
 
             // Create commands
             ExitCommand = new RelayCommand(Exit);
@@ -90,6 +103,87 @@ namespace Testinator.Client.Core
 
             // Get user answers
             UserAnswers = IoCClient.TestHost.UserAnswers;
+        }
+
+        /// <summary>
+        /// Compares the both answers and questions lists and calculates user point score based on that
+        /// </summary>
+        /// <returns></returns>
+        private int CompareUserAnswersAndCalculatePoints()
+        {
+            // Keep track of user total point score
+            var totalScore = 0;
+
+            // For each question...
+            foreach (var question in Questions)
+            {
+                // Look for the match answer
+                foreach (var answer in UserAnswers)
+                    if (answer.ID == question.ID)
+                    {
+                        // Based on question type...
+                        switch (question.Type)
+                        {
+                            case QuestionType.MultipleChoice:
+                                {
+                                    // Cast the answer and question objects
+                                    var multipleChoiceAnswer = answer as MultipleChoiceAnswer;
+                                    var multipleChoiceQuestion = question as MultipleChoiceQuestion;
+
+                                    // Check if user has answered correctly
+                                    if (multipleChoiceQuestion.CorrectAnswerIndex == multipleChoiceAnswer.SelectedAnswerIdx)
+                                        // Give him points for this question
+                                        totalScore += multipleChoiceQuestion.PointScore;
+                                }
+                                break;
+
+                            case QuestionType.MultipleCheckboxes:
+                                {
+                                    // Cast the answer and question objects
+                                    var multipleCheckboxesAnswer = answer as MultipleCheckboxesAnswer;
+                                    var multipleCheckboxesQuestion = question as MultipleCheckboxesQuestion;
+
+                                    // TODO: Advanced checkboxes logic (giving points for every good answer, or only for the whole set etc.)
+                                }
+                                break;
+
+                            case QuestionType.SingleTextBox:
+                                {
+                                    // Cast the answer and question objects
+                                    var singleTextBoxAnswer = answer as SingleTextBoxAnswer;
+                                    var singleTextBoxQuestion = question as SingleTextBoxQuestion;
+
+                                    // Catch the right answer string and user answer string
+                                    var rightAnswer = singleTextBoxAnswer.Answer;
+                                    var userAnswer = singleTextBoxQuestion.CorrectAnswer;
+
+                                    // Remove white spaces from them
+                                    rightAnswer = rightAnswer.Trim();
+                                    userAnswer = userAnswer.Trim();
+
+                                    // TODO: Check in settings if it the answer has to be case sensitive
+                                    if (!false)//isCaseSensitivityImportant?
+                                    {
+                                        // Answer doesn't have to be case sensitive, make every string as lowercase
+                                        rightAnswer = rightAnswer.ToLower();
+                                        userAnswer = userAnswer.ToLower();
+                                    }
+
+                                    // Check if answer matches
+                                    if (rightAnswer == userAnswer)
+                                        // Give user points for this question
+                                        totalScore += singleTextBoxQuestion.PointScore;
+                                }
+                                break;
+                        }
+
+                        // We have handled answer for this question, go to the next one
+                        break;
+                    }
+            }
+
+            // Return calculated point score
+            return totalScore;
         }
 
         #endregion
---
+++ Source/Testinator.Client/ValueConverters/MarkToTextConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Testinator.Core;
+using Testinator.UICore;
+
+namespace Testinator.Client
+{
+    /// <summary>
+    /// A converter that takes in a <see cref="Marks"/>
+    /// </summary>
+    public class MarkToTextConverter : BaseValueConverter<MarkToTextConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Cast value to Mark enumerator
+            var mark = (Marks)value;
+
+            // Based on mark...
+            switch(mark)
+            {
+                case Marks.A:
+                    return LocalizationResource.MarkAName;
+                case Marks.B:
+                    return LocalizationResource.MarkBName;
+                case Marks.C:
+                    return LocalizationResource.MarkCName;
+                case Marks.D:
+                    return LocalizationResource.MarkDName;
+                case Marks.E:
+                    return LocalizationResource.MarkEName;
+                case Marks.F:
+                    return LocalizationResource.MarkFName;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
