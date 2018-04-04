9bee9d25fb26585ceae9e6986a6e0006bf23534d
Hooterr <maxus1999@gmail.com>
2018-04-04 07:33:31 +0200

    File system base

---
+++ Source/Testinator.Core/Files/Base/IFileManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Testinator.Core
+{
+    public interface IFileManager
+    {
+        void WriteToFile(string FileName, string FilePath, byte[] Content);
+
+        void DeleteFile(string FileName, string FilePath);
+
+        byte[] ReadFile(string FileName, string FilePath);
+    }
+}
--- Source/Testinator.UnitTests/Helper.cs
+++ Source/Testinator.UnitTests/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Testinator.UnitTests
 {
