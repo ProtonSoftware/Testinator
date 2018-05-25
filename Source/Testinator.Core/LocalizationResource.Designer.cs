65c01c99f5ad85e87e554d883d4580e7aa2697c0
Patryk Mikulski <minorsonek@gmail.com>
2018-05-25 12:46:30 +0200

    localization files fix

--- Source/Testinator.Core/LocalizationResource.Designer.cs
+++ Source/Testinator.Core/LocalizationResource.Designer.cs
@@ -39,7 +39,7 @@ namespace Testinator.Core {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Testinator.Core.Localization.LocalizationResource", typeof(LocalizationResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Testinator.Core.LocalizationResource", typeof(LocalizationResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
